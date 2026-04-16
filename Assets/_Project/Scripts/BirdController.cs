1c6d34b05bc38d5cc44d0fb987822ebd465aac79
Trybal <cedricstourme@gmail.com>
2026-04-16 15:58:00 +0200

    [DEV] animations and transitions : work

--- Assets/_Project/Scripts/BirdController.cs
+++ Assets/_Project/Scripts/BirdController.cs
@@ -5,10 +5,11 @@ using UnityEngine.InputSystem;
 public class BirdController : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    [Header("Bird Settings"), Space(10)]
     public float jumpForce = 0;
     private Camera _camera;
     [HideInInspector] public GameManager m_manager;
-
     [SerializeField]private bool isDead;
     
     // Création d'une variable pour stocker l'état actuel
@@ -16,19 +17,20 @@ public class BirdController : MonoBehaviour
     // On commence en "Playing"
     private GameState currentState = GameState.Playing;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         _camera = Camera.main;
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
         if (_rigidbody2D == null) Debug.LogError("BirdController: No Rigidbody2D found!");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         // On ne fait rien si on n'est pas en train de jouer
-        if (currentState != GameState.Playing) return;
+        if (currentState == GameState.Playing)
         {
             // Si j'appuie sur barre d'espace, le joueur saute.
             HandleInput();
@@ -68,14 +70,16 @@ public class BirdController : MonoBehaviour
     
         currentState = GameState.GameOver;
         m_manager.GameOver();
-        // On ne met pas "enabled = false" ici, sinon l'oiseau s'arrête de tomber net
     }
 
     private void HandleInput()
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
+            // On applique la force de saut
             _rigidbody2D.AddForceY(jumpForce, ForceMode2D.Impulse);
+            // On déclenche l'animation via le Trigger "FlyTrigger"
+            _animator.SetTrigger("FlyTrigger");
         }
     }
 
